31d6de7edb4b74a7bf992664ebd8d0ce9f27509b
PremanandChowdhury <freudigprem@gmail.com>
2021-11-09 23:25:47 +0530

    feat: Angular Httpclient and Fetching data from server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private httpUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = TASKS;
-    return of(tasks);
+    return this.http.get<Task[]>(this.httpUrl);
   }
 }
