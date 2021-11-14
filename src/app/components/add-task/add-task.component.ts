199a93b10d67846e5ff6095640e5b962169d304c
PremanandChowdhury <freudigprem@gmail.com>
2021-11-14 15:07:45 +0530

    code: Submit functionality for the form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -6,11 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
-  text: string;
-  day: string;
+  text!: string;
+  day!: string;
   reminder: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please enter a task!');
+      return;
+    }
+
+    const newTask = () => {
+      text: this.text;
+      day: this.day;
+      reminder: this.reminder;
+    };
+
+    // TODO: emit event
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
