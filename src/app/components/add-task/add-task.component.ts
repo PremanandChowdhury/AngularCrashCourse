83bf870deae245fa7b00943cece69e46f24ee6d1
PremanandChowdhury <freudigprem@gmail.com>
2021-11-15 23:27:15 +0530

    code: Added event Emitter

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
   text!: string;
   day!: string;
   reminder: boolean = false;
