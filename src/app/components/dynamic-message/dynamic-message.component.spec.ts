20c1d1ad45abba16ce07478be97fd661caf0eb66
PremanandChowdhury <freudigprem@gmail.com>
2021-11-21 23:48:20 +0530

    feat: Dynamic Message Component Created

---
+++ src/app/components/dynamic-message/dynamic-message.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DynamicMessageComponent } from './dynamic-message.component';
+
+describe('DynamicMessageComponent', () => {
+  let component: DynamicMessageComponent;
+  let fixture: ComponentFixture<DynamicMessageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DynamicMessageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DynamicMessageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/dynamic-message/dynamic-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-message',
+  templateUrl: './dynamic-message.component.html',
+  styleUrls: ['./dynamic-message.component.css']
+})
+export class DynamicMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
