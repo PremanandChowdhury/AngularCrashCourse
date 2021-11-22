750a187375d43595d29b7b3666b25577c62f72fb
PremanandChowdhury <freudigprem@gmail.com>
2021-11-22 23:07:58 +0530

    feat: Get the  values of an excel sheet

--- src/app/components/dynamic-message/dynamic-message.component.ts
+++ src/app/components/dynamic-message/dynamic-message.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import * as xlsx from 'xlsx';
 
+type AOA = any[][];
 @Component({
   selector: 'app-dynamic-message',
   templateUrl: './dynamic-message.component.html',
-  styleUrls: ['./dynamic-message.component.css']
+  styleUrls: ['./dynamic-message.component.css'],
 })
 export class DynamicMessageComponent implements OnInit {
+  data: any = [];
 
-  constructor() { }
+  wopts: xlsx.WritingOptions = {
+    bookType: 'xlsx',
+    type: 'array',
+  };
 
-  ngOnInit(): void {
+  onFileChange(e: any) {
+    // Wire up the reader
+    const target: DataTransfer = <DataTransfer>e.target;
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      // read workbook
+      const bstr: string = e.target.result;
+      const wb: xlsx.WorkBook = xlsx.read(bstr, {
+        type: 'binary',
+      });
+
+      // Grab first sheet
+      const wsname: string = wb.SheetNames[0];
+      const ws: xlsx.WorkSheet = wb.Sheets[wsname];
+
+      // save data
+      this.data = <AOA>xlsx.utils.sheet_to_json(ws, {
+        header: 1,
+      });
+
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
